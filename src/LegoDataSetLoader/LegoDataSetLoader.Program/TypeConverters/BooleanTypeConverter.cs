b5fa0d5584bcd2bba72d84b48b4ad3471b94a34c
Dan Mallott <daniel.mallott@gmail.com>
2026-05-06 12:25:53 -0500

    Update to slnx and .NET 10

--- src/LegoDataSetLoader/LegoDataSetLoader.Program/TypeConverters/BooleanTypeConverter.cs
+++ src/LegoDataSetLoader/LegoDataSetLoader.Program/TypeConverters/BooleanTypeConverter.cs
@@ -6,12 +6,12 @@ namespace LegoDataSetLoader.Program.TypeConverters;
 
 public class BooleanTypeConverter : DefaultTypeConverter
 {
-    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    public override object ConvertFromString(string? text, IReaderRow row, MemberMapData memberMapData)
     {
-        return text.ToLowerInvariant() == "t";
+        return text?.ToLowerInvariant() == "t";
     }
 
-    public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+    public override string ConvertToString(object? value, IWriterRow row, MemberMapData memberMapData)
     {
         bool? valueAsBool = value as bool?;
         return valueAsBool.HasValue && valueAsBool.Value ? "t" : "f";
